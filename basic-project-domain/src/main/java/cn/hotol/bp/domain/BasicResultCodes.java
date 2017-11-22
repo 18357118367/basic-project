5728a52fdef99528ff663e31870a4e96e9cf539a
zhagnzhihao <zhang950730@gmail.com>
2017-11-22 21:18:01 +0800

    BasicResult

--- basic-project-domain/src/main/java/cn/hotol/bp/domain/BasicResultCodes.java
+++ basic-project-domain/src/main/java/cn/hotol/bp/domain/BasicResultCodes.java
@@ -3,28 +3,17 @@ package cn.hotol.bp.domain;
 
 public enum BasicResultCodes {
     SUCCESS(0, "成功"),
+    ERROR(-1, "失败"),
     SYS_EXCEPTION(1, "系统异常"),
-    PARAMETER_EMPTY(2,"参数为空"),
-    ADD_FAIL(3,"新增失败"),
+    PARAMETER_EMPTY(2, "参数为空"),
+    ADD_FAIL(3, "新增失败"),
     UPDATE_FAIL(4, "修改失败"),
     DELETE_FAIL(5, "删除失败"),
-    PARAMETER_INVALID(6,"参数错误"),
-    DATA_EXISTS(7,"数据已存在"),
+    PARAMETER_INVALID(6, "参数错误"),
+    DATA_EXISTS(7, "数据已存在"),
     UNDEFIND_ERROR(8, "未知错误"),
     DATA_ERROR(9, "数据错误"),
-    DATA_NOT_EXIST(10,"该数据不存在"),
-
-
-
-
-
-
-
-
-
-
-
-
+    DATA_NOT_EXIST(10, "该数据不存在"),
 
 
     XXXXX(-10000000, "XXXX");
