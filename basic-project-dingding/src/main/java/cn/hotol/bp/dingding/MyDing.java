612eaf286e94de26b3f3e14ec199e438d3ba90ee
liulinfeng <17520463943@163.com>
2018-02-02 11:07:59 +0800

    增加basic-project-dingding功能模块

---
+++ basic-project-dingding/src/main/java/cn/hotol/bp/dingding/MyDing.java
@@ -0,0 +1,55 @@
+package cn.hotol.bp.dingding;
+
+import com.dingtalk.chatbot.DingtalkChatbotClient;
+import com.dingtalk.chatbot.SendResult;
+import com.dingtalk.chatbot.message.Message;
+import com.dingtalk.chatbot.message.TextMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class MyDing {
+    private final static Logger logger = LoggerFactory.getLogger(MyDing.class);
+    private String hook;
+    private DingtalkChatbotClient client;
+
+    //发送钉钉消息
+    public String sendTextMessage(String message){
+        TextMessage textMessage = new TextMessage(message);
+        SendResult result = null;
+        try {
+            result = client.send(hook,textMessage);
+            return result.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error("【钉钉消息发送】错误原因："+e.getMessage());
+        }
+        return null;
+    }
+
+    public void init(){
+        logger.info("钉钉初始化");
+        client = new DingtalkChatbotClient();
+    }
+
+    public void destroy(){
+    logger.info("钉钉销毁");
+    }
+
+    public String getHook() {
+        return hook;
+    }
+
+    public void setHook(String hook) {
+        this.hook = hook;
+    }
+
+    public DingtalkChatbotClient getClient() {
+        return client;
+    }
+
+    public void setClient(DingtalkChatbotClient client) {
+        this.client = client;
+    }
+}
