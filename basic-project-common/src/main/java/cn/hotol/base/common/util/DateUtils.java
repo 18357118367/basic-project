09f446e8b222388cf6c06c71aa9eeb0c633a6810
zhagnzhihao <zhang950730@gmail.com>
2018-02-05 21:22:35 +0800

    优化计算俩时间天数方法

--- basic-project-common/src/main/java/cn/hotol/base/common/util/DateUtils.java
+++ basic-project-common/src/main/java/cn/hotol/base/common/util/DateUtils.java
@@ -69,6 +69,7 @@ public class DateUtils {
         c.set(Calendar.HOUR_OF_DAY, 0);
         c.set(Calendar.MINUTE, 0);
         c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
         return c.getTime();
     }
 
@@ -115,36 +116,36 @@ public class DateUtils {
     /**
      * 计算两个日期相差的月份数
      *
-     * @param date1 日期1
-     * @param date2 日期2
-     * @param pattern  日期1和日期2的日期格式
-     * @return  相差的月份数
+     * @param date1   日期1
+     * @param date2   日期2
+     * @param pattern 日期1和日期2的日期格式
+     * @return 相差的月份数
      * @throws ParseException
      */
-    public static int countMonths(String date1,String date2,String pattern) throws ParseException {
-        SimpleDateFormat sdf=new SimpleDateFormat(pattern);
-        Calendar c1=Calendar.getInstance();
-        Calendar c2=Calendar.getInstance();
+    public static int countMonths(String date1, String date2, String pattern) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        Calendar c1 = Calendar.getInstance();
+        Calendar c2 = Calendar.getInstance();
         c1.setTime(sdf.parse(date1));
         c2.setTime(sdf.parse(date2));
-        int year =c2.get(Calendar.YEAR)-c1.get(Calendar.YEAR);
+        int year = c2.get(Calendar.YEAR) - c1.get(Calendar.YEAR);
         //开始日期若小月结束日期
-        if(year<0){
-            year=-year;
-            return year*12+c1.get(Calendar.MONTH)-c2.get(Calendar.MONTH);
+        if (year < 0) {
+            year = -year;
+            return year * 12 + c1.get(Calendar.MONTH) - c2.get(Calendar.MONTH);
         }
-        return year*12+c2.get(Calendar.MONTH)-c1.get(Calendar.MONTH);
+        return year * 12 + c2.get(Calendar.MONTH) - c1.get(Calendar.MONTH);
     }
 
     /**
-     * @description     获取当前日期，以字符串形式返回
-     * @param pattern   日期格式
-     * @return          当前日期 （字符串）
+     * @param pattern 日期格式
+     * @return 当前日期 （字符串）
+     * @description 获取当前日期，以字符串形式返回
      * @Date 2018/1/22 14:52
      */
-    public static String getCurrentDateStr(String pattern){
-        SimpleDateFormat sdf=new SimpleDateFormat(pattern);
-        Date date=new Date();
+    public static String getCurrentDateStr(String pattern) {
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        Date date = new Date();
         return sdf.format(date);
     }
 
@@ -155,14 +156,14 @@ public class DateUtils {
      * @param date2
      * @return date2>=date1 true ;date2<date1 false
      */
-    public static Boolean compareWithDay(Date date1,Date date2){
-        if (date1 == null || date2 == null){
+    public static Boolean compareWithDay(Date date1, Date date2) {
+        if (date1 == null || date2 == null) {
             return false;
         }
         SimpleDateFormat dateFormat = new SimpleDateFormat(SIMPLE_FORMAT);
         String one = dateFormat.format(date1);
         String two = dateFormat.format(date2);
-        if (one.compareTo(two) <= 0){
+        if (one.compareTo(two) <= 0) {
             return true;
         }
         return false;
